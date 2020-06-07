3cce44d824167bc79e11f4d4b42375898dc91173
Kryzys09 <Kryzysk09@gmail.com>
2020-06-07 16:49:30 +0200

    aktualizacja domyślnych dat kalkulatora

--- SpiritGuard/alcohols/views.py
+++ SpiritGuard/alcohols/views.py
@@ -15,16 +15,25 @@ def render_calculator(request):
     user = request.session['user']
     user_data = db.child('users').child(user['localId']).get().val()
     print("USER DATA: ", user_data)
-    drinks = get_drinks(user_data['logs'])
-    if user_data['gender'] == "male":
-        gender = 1
+    if 'logs' in user_data.keys():
+        drinks = get_drinks(user_data['logs'])
+    else:
+        drinks = []
+    if user_data['gender'] == "M":
+        gender = 0
     else:
         gender = 0
     bac = blood_alcohol_content(gender, drinks, user_data['weight'], 1)
-    mai = max_alcohol_intake(gender, datetime.datetime.now() + datetime.timedelta(hours=8), datetime.datetime.now(), bac)
+    now = datetime.datetime.now() + datetime.timedelta(hours=2)
+    then = now + datetime.timedelta(hours=8)
+    mai = max_alcohol_intake(gender, then, now, bac)
     bmi = get_bmi(user_data['weight'], user_data['height'])
     data = {
         'bac':  "{:.3f}".format(bac),
+        'date_of_first_drink': "{d}-{m}-{y}".format(d=now.date().day, m=now.date().month, y=now.date().year),
+        'date_of_last_drink': "{d}-{m}-{y}".format(d=then.date().day, m=then.date().month, y=then.date().year),
+        'time_of_first_drink': "{h}:{m}".format(h=now.time().hour, m=now.time().minute),
+        'time_of_last_drink': "{h}:{m}".format(h=then.time().hour, m=then.time().minute),
         'sobering_time': sobering_time_projection(15, gender, bac).popitem()[0],
         'max_alcohol_intake': "{:.3f}".format(mai),
         'translate_bac': translate_bac(gender, user_data['weight'], mai, classic_alcohols),
